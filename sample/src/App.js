38c166a220bf2e8d9b398fa337eebcf71cc8e8bb
Quentin Briand <quentin.briand@epitech.eu>
2022-03-23 00:06:35 +0100

    First commit

---
+++ sample/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './App.css';
+import Login from './components/login';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>React Apps WOW</h1>
+        <Login/>
+    </div>
+  );
+}
+
+export default App;
---
+++ sample/src/components/login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Grid } from "@material-ui/core"
+
+const Login=()=>{
+	return (
+		<Grid>
+			<Paper elevation={10}> Sign in</Paper>
+		</Grid>
+	)
+}
+
+export default Login

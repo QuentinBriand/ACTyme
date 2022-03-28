5c6a0ea9500097b2846dcc9e062cf5e5e026ce9d
Quentin Briand <quentin.briand@epitech.eu>
2022-03-28 12:16:09 +0200

    Logged in

--- sample/src/components/loggedIn.js
+++ sample/src/components/loggedIn.js
@@ -1,5 +1,4 @@
-/* eslint-disable jsx-a11y/anchor-has-content */
-/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
 import Logo from "./logo-act-no-text.svg";
 import "./loggedIn.css";
 

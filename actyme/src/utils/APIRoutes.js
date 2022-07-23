174bb8d34b20e88906d83430e3bbea00fec47d7c
Quentin Briand <quentin.briand@epitech.eu>
2022-07-23 12:54:38 +0200

    Cleaned all

--- actyme/src/utils/APIRoutes.js
+++
@@ -1,4 +0,0 @@
-export const server = "http://localhost:7000";
-export const registerRouteAPI = `${server}/api/auth/register`;
-export const loginRouteAPI = `${server}/api/auth/login`;
-export const getUserRouteAPI = `${server}/api/users`;
--- actyme/src/utils/ErrorStyle.js
+++
@@ -1,9 +0,0 @@
-const ErrorStyle = {
-    position: "bottom-right",
-    autoClose: 2000,
-    pauseOnHover: false,
-    draggable: false,
-    theme: "dark"
-};
-
-export default ErrorStyle;
